600e7e45626b35577f6bc05d4427080bc0d20001
czPechy <martin@pecha.pro>
2021-04-19 12:06:02 +0200

    Create auto-removeFriends.js

---
+++ facebook/auto-removeFriends.js
@@ -0,0 +1,13 @@
+document.jq = $;
+function removeFriend() {
+	document.jq('[aria-label="Friends"][role=button]').click()
+	setTimeout(function(){ 
+		document.jq('[role=menuitem]:last-child').click(); 
+		setTimeout(function(){ 
+			document.jq('[aria-label="Confirm"]').click()
+			setTimeout(function(){ 
+				removeFriend();
+			}, 500);
+		}, 500);
+	}, 1000);
+}
